21d056ab3d89d3ed4c23bdf3ac3266c7ca56753e
Gerhard van der Linde <G00364778@gmit.ie>
2019-10-26 18:05:21 +0100

    rename scraping code folder to 03scraping

---
+++ code/03scraping/web_scraping_to_csv.py
@@ -0,0 +1,27 @@
+import requests
+import csv
+from bs4 import BeautifulSoup
+url = "https://www.myhome.ie/residential/mayo/property-for-sale?page=2"
+# load the page from the above url in the object page.
+page = requests.get(url)
+#parse only the html from page.content
+soup = BeautifulSoup(page.content, 'html.parser')
+#open a file and csv writer objects
+home_file = open('week03MyHome.csv', mode='w', newline='\n', encoding='1252')#encoding='UTF-8'
+home_writer = csv.writer(home_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+#parse the listings from soup using the class label
+listings = soup.findAll("div", class_="PropertyListingCard" )
+#loop trhough the listings
+for listing in listings:
+    #create an empty list for writing out to csv
+    entryList = []
+    #parse and append price to listing
+    price = listing.find(class_="PropertyListingCard__Price").text.strip()
+    entryList.append(price)
+    #parse and append address to listing
+    address = listing.find(class_="PropertyListingCard__Address").text.strip()
+    entryList.append(address)
+    #write parsed data to the csv file
+    home_writer.writerow(entryList)
+#close the file
+home_file.close()
